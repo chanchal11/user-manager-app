9650b2282b2b6cf0c30ff6e4c25bb0bb3d11aead
Chanchal Kumar <rrabshrivastava@gmail.com>
2024-03-29 21:54:08 +0530

    dark & light mode theme added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,46 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import UserTable from './components/UserTable';
 import Toolbar from './components/Toolbar';
-import { Container } from '@mui/material';
+import { Button, Container, createTheme, CssBaseline, ThemeProvider, useMediaQuery } from '@mui/material';
+
+
 
 const App: React.FC = () => {
+  const isDarkModeInSystem = useMediaQuery('(prefers-color-scheme: dark)');
+  const [ mode, setMode ] =  useState(isDarkModeInSystem ? 'dark' : 'light');
+  const darkTheme = createTheme({ 
+    palette: { 
+        mode: mode == 'dark'  ? 'dark' : 'light'
+    }, 
+}); 
+
+useEffect(() => {
+  if(isDarkModeInSystem)
+    setMode('dark');
+  else
+    setMode('light');
+}, [ isDarkModeInSystem ] );
+
   return (
     <Provider store={store}>
-      <Container maxWidth="lg" className='container' >
-        <Toolbar />
-        <UserTable />
-      </Container>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Container maxWidth="lg" className='container' >
+          <Toolbar />
+          <UserTable />
+        </Container>
+        <div className='themeModeButton' >
+        <Button
+          variant="contained"
+          onClick={() => setMode(mode === 'light' ? 'dark' : 'light')}
+        >
+          {darkTheme.palette.mode === 'dark' ? 'Light Mode' : 'Dark Mode'}
+        </Button>
+        </div>
+      </ThemeProvider>
     </Provider>
   );
 };
